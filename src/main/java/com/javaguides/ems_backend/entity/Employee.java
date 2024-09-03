0471e7b9bf1216ff4a3d9e877ef4dba83e1578e8
AkhilPolisetty <akhilpolisetty456@gmail.com>
2024-09-03 21:09:01 +0530

    Employee dto and Employee mapper created

--- src/main/java/com/javaguides/ems_backend/entity/Employee.java
+++ src/main/java/com/javaguides/ems_backend/entity/Employee.java
@@ -1,10 +1,8 @@
 package com.javaguides.ems_backend.entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
+import org.antlr.v4.runtime.misc.NotNull;
 
 @Getter
 @Setter
@@ -12,16 +10,59 @@ import lombok.Setter;
 @AllArgsConstructor
 @Entity
 @Table(name="employees")
+@Data
+
 public class Employee {
+
+
 
     @Id
     @GeneratedValue(strategy= GenerationType.IDENTITY)
     private Long id;
-    @Column(name="first_name")
+
+
+
+    @Column(name="first_name",nullable=false)
     private String firstName;
-    @Column(name="last_name")
+
+
+
+    @Column(name="last_name",nullable=false)
     private String lastName;
     @Column(name="email_id",nullable=false,unique=true)
     private String email;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
 
 }
---
+++ src/main/java/com/javaguides/ems_backend/mapper/EmployeeMapper.java
@@ -0,0 +1,14 @@
+package com.javaguides.ems_backend.mapper;
+
+import com.javaguides.ems_backend.dto.EmployeeDto;
+import com.javaguides.ems_backend.entity.Employee;
+
+public class EmployeeMapper {
+    public static EmployeeDto mapToEmployeeDto(Employee employee){
+        return new EmployeeDto(
+
+
+
+        );
+    }
+}
