6ed34a16a5469f6a3b23303c018b0f114c830567
AkhilPolisetty <akhilpolisetty456@gmail.com>
2024-09-03 21:22:10 +0530

    Hello

--- src/main/java/com/javaguides/ems_backend/dto/EmployeeDto.java
+++ src/main/java/com/javaguides/ems_backend/dto/EmployeeDto.java
@@ -17,14 +17,35 @@ public class EmployeeDto {
     private String email;
 
 
+    public Long getId() {
+        return id;
+    }
 
+    public String getFirstName() {
+        return firstName;
+    }
 
+    public String getLastName() {
+        return lastName;
+    }
 
+    public String getEmail() {
+        return email;
+    }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
 
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
 
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 
-
-
-
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
